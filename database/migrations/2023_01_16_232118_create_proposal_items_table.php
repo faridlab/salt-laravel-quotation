a3f5e54aff45bb67a112a47309afe994f94a8a17
Farid Hidayat <e.faridhidayat@gmail.com>
2023-01-17 00:33:22 +0700

    feat(migrate): proposal_items

---
+++ database/migrations/2023_01_16_232118_create_proposal_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposal_items', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('proposal_id')->references('id')->on('proposals');
+
+            $table->text('title');
+            $table->text('description')->nullable();
+            $table->json('data')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposal_items');
+    }
+};
