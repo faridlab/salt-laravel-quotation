22b89cc72c68a2a794cd9c8803450964227c621e
Farid Hidayat <e.faridhidayat@gmail.com>
2023-01-17 22:57:51 +0700

    feat(model): Sections

---
+++ src/Models/Sections.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace SaltQuotation\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+
+class Sections extends Resources {
+
+    protected $table = 'proposal_sections';
+
+    use Uuids;
+    use ObservableModel;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table provinces
+        'id',
+        'proposal_id',
+        'title',
+        'description',
+        'data',
+        'order',
+    ];
+
+    protected $rules = array(
+        'proposal_id' => 'required|string',
+        'title' => 'required|string',
+        'description' => 'nullable|string',
+        'data' => 'nullable|json',
+        'order' => 'nullable|integer',
+    );
+
+    protected $auths = array (
+        'index',
+        'store',
+        'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $forms = array();
+    protected $structures = array();
+    protected $searchable = array(
+        'proposal_id',
+        'title',
+        'description',
+        'data',
+        'order',
+    );
+
+    protected $fillable = array(
+        'proposal_id',
+        'title',
+        'description',
+        'data',
+        'order',
+    );
+}
